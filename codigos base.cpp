dda368cb32ca726eb382ef89a2ec5f6ab9fece6a
Alfaro <diegopena5294@gmail.com>
2025-09-16 21:14:44 -0600

    Add files via upload

---
+++ codigos base.cpp
@@ -0,0 +1,98 @@
+/*primer codigo de hola mundo*/               
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
+
+/*segundo codigo de hola mundo*/               
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
+
+/*tercer codigo de hola mundo*/
+
+
+#include <iostream>
+
+using namespace std;
+
+void hola()
+{
+    cout<<"Hello World";
+}
+int main()
+{
+    hola();
+    
+    return 0;
+}
+
+/*primer codigo de suma*/
+
+#include <iostream>
+int a=5;
+int b=2;
+int main()
+{
+    int r=a+b;
+    std::cout <<"la suma es:"<<r;
+    
+    return 0;
+}
+
+/*segundo codigo de suma*/
+
+#include <iostream>
+int a=5;
+int b=2;
+int r;
+int main()
+{
+    int r=a+b;
+    std::cout <<"la suma es:"<<r;
+    
+    return 0;
+}
+/*tercer codigo de suma*/
+
+#include <iostream>
+int a=5;
+int b=2;
+int r=0;
+int main()
+{
+    int r=a+b;
+    std::cout <<"la suma es:"<<r;
+    
+    return 0;
+}
+/*cuarto codigo*/
+
+#include <iostream>
+using namespace std;
+int a=5;
+int b=2;
+int r=0;
+void suma()
+{
+    r=a+b;
+    std::cout <<"la suma es:"<<r;
+}
+int main()
+{
+suma();
+    return 0;
+}
